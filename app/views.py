29d4e5f3fba38866683f1d5067251aea1292d696
Nvnrocky <narasimhanagulapati01@gmail.com>
2023-11-09 22:42:35 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def macha1(request):
+    return render(request,'macha1.html')
+
+def macha2(request):
+    return render(request,'macha2.html')
+
+def macha3(request):
+    return render(request,'macha3.html')
+
+def macha4(request):
+    return render(request,'macha4.html')
